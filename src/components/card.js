e066be2ce0e06d5e1641e19536ea912d3d526f74
axel669 <haryuu.hanekata@gmail.com>
2018-06-20 11:27:58 -0700

    finished card with most of the material design parts

--- src/components/card.js
+++ src/components/card.js
@@ -13,41 +13,73 @@ style.add({
             '-2px 0px 2px rgba(0, 0, 0, 0.25)',
             '0px -2px 2px rgba(0, 0, 0, 0.25)'
         ].join(', '),
-        backgroundColor: 'white',
+        backgroundColor: theme.card.bg,
         overflow: 'hidden',
-        borderRadius: 5,
+        borderRadius: 2,
+        padding: 12,
         position: 'relative',
         top: 0,
         left: 0
     },
-    "doric-card[side-img='true']": {
-        paddingRight: 80
+    "doric-card > :first-child": {
+        marginTop: -12
     },
-    "doric-card > card-content": {
+    "doric-card > :last-child": {
+        marginBottom: -12
+    },
+    "doric-card > [data-card-flush], doric-card-media": {
+        marginLeft: -12,
+        marginRight: -12
+    },
+    "doric-card-title, doric-card-actions": {
         display: 'block',
-        padding: 3
+        margin: '12px 0px',
+        padding: 12
     },
-    "doric-card > card-title": {
+    "doric-card-media + doric-card-title": {
+        marginTop: 0
+    },
+    "doric-card-title + doric-card-media": {
+        marginTop: -12
+    },
+    "doric-card-media": {
+        height: 180,
         display: 'block',
-        padding: 5,
-        fontSize: 20,
-        backgroundColor: theme.card.title.bg,
-        color: theme.card.title.color
+        marginBottom: 12
     },
-    "doric-card > card-action": {
-        display: 'block',
-        padding: 5
+    "doric-card-title::before": {
+        content: `" "`,
+        display: 'table'
     },
-    "doric-card > doric-card-side-image": {
-        position: 'absolute',
-        left: 'auto',
-        right: 0,
-        top: 0,
-        bottom: 0,
-        width: 80
+    "doric-card-title::after": {
+        content: `" "`,
+        display: 'table',
+        clear: 'both'
+    },
+    "doric-card > doric-card-actions[data-divider]": {
+        borderTop: `2px solid ${theme.divider.color}`
+    },
+    "doric-card-actions :first-child": {
+        marginLeft: 0
+    },
+    ".doric-title-main": {
+        fontSize: 22,
+        marginBottom: 8
+    },
+    ".doric-title-subtitle": {
+        float: 'left',
+        color: '#666'
+    },
+    ".doric-title-icon": {
+        width: 48,
+        height: 48,
+        float: 'left',
+        borderRadius: 24,
+        marginRight: 8,
+        overflow: 'hidden'
     }
 });
-export default props => {
+const Card = props => {
     const {
         title = null,
         sideImage = null,
@@ -77,3 +109,29 @@ export default props => {
         </doric-card>
     );
 };
+Card.actions = props => {
+    const divider = props.divider;
+
+    return (
+        <doric-card-actions data-card-flush data-divider={divider}>
+            {props.children}
+        </doric-card-actions>
+    );
+};
+Card.media = props => <doric-card-media {...props} />;
+Card.title = props => {
+    const {main, subtitle, icon = null} = props;
+    const iconElement = (icon === null)
+        ? null
+        : <div className="doric-title-icon"><Image source={icon} width="100%" height="100%" imageSize="cover" /></div>;
+
+    return (
+        <doric-card-title data-card-flush>
+            {iconElement}
+            <div className="doric-title-main">{main}</div>
+            <div className="doric-title-subtitle">{subtitle}</div>
+        </doric-card-title>
+    );
+};
+
+export default Card;
